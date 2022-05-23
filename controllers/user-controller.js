40bb679baf879d0dd455e758d46dfde44b798040
Chris <christopher.sarm15@gmail.com>
2022-05-23 16:38:09 -0400

    updatng user controllers for post and put

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,7 @@
 const { User } = require("../models");
 
 const userController = {
-  // get all users
+  // get all users - api/users
   getAllUsers(req, res) {
     User.find({})
       //including thoughts and friends of the user for the user query
@@ -22,6 +22,54 @@ const userController = {
         res.status(400).json(err);
       });
   },
+
+  //get user by id - api/users/:id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      //including thoughts and friends of the user for the user query
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        //if no user found
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.satuts(400).json(err);
+      });
+  },
+
+  //create a user, POST- api/users
+  createUser({ body }, res) {
+      User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.satuts(400).json(err);
+        })
+  },
+
+  // update users by id PUT- api/users/:id
+  updateUser({ params, body }, res) {
+      User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: "No user found with this id!"});
+            }
+            res.json(dbUserData);
+        })
+  }
+
 };
 
 module.exports = userController;
