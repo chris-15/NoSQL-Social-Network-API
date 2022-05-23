2c089e00278a075e083382e4dcfc2ff912872a8b
Chris <christopher.sarm15@gmail.com>
2022-05-23 15:40:57 -0400

    created the reaction schema for reactions on thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,41 @@
 const { Schema, model, Types} = require("mongoose");
+// requiring moment to format createdAt dates
 const moment = require('moment');
 
+
+// reaction model schema
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: [280, "Reaction must be 280 characters or less!"]
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format("MMM Do YYYY, h:mm a")
+        }
+    }
+);
+
+//thought model schema
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
             required: true,
+            //validaton for length requirement
             minlength: [1, "Thought must be atleast 1 character long!"],
-            maxLength: [280, "Thought must be less 280 characters long!"]
+            maxLength: [280, "Thought must be 280 characters or less!"]
         },
         createdAt: {
             type: Date,
@@ -16,12 +44,10 @@ const ThoughtSchema = new Schema(
             get: (createdAtVal) => moment(createdAtVal).format("MMM Do YYYY, h:mm a")
 
         },
-        username: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ],
+        username: {
+            type: String,
+            required: true
+        },
         //using ReactionSchema to validate data for a reaction
         reactions: [ReactionSchema]
     },
@@ -33,7 +59,7 @@ const ThoughtSchema = new Schema(
         id: false
     }
 );
-
+// virtual that returns how many reactions a thought has 
 ThoughtSchema.virtual('reactionCount', ThoughtSchema).get(function() {
     return this.reactions.length;
 })
